8308ea7103aa62929e607af70e8452eba9b45fb6
YaoChuWu <yaochuwu.tw@gmail.com>
2024-09-30 05:00:36 +0800

    Create git_filter_urls.sh

---
+++ git_filter_urls.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## URLS that HDF5 Needed
+## 
+URLS_BITSHUFFLE="https://github.com/kiyo-masui/bitshuffle.git"
+URLS_CBLOSC="https://github.com/Blosc/c-blosc.git"
+URLS_ZLIB="https://github.com/madler/zlib.git"
+URLS_CBLOSC2="https://github.com/Blosc/c-blosc2.git"
+URLS_BZIP2="https://github.com/libarchive/bzip2.git"
+URLS_FPZIP="https://github.com/LLNL/fpzip.git"
+URLS_LIBJPEG-TURBO="https://github.com/libjpeg-turbo/libjpeg-turbo.git"
+URLS_LZ4="https://github.com/lz4/lz4.git"
+TAR_LIBLZF="https://dist.schmorp.de/liblzf/liblzf-3.6.tar.gz"
+URLS_SZ="https://github.com/szcompressor/SZ.git"
+URLS_ZFP="https://github.com/LLNL/zfp.git"
+URLS_ZSTD="https://github.com/facebook/zstd.git"
+
