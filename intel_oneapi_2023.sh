44968aecd5dcde7abedfbb07423c0a1af1879e25
YaoChuWu <yaochuwu.tw@gmail.com>
2024-11-17 02:50:14 +0800

    Create intel_oneapi_2023.sh

---
+++ intel_oneapi_2023.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# TARGET 
+# Setting up path to reach the offline scripts
+# 
+
+install_script_base="l_BaseKit_p_2024.2.1.100_offline.sh"
+install_script_hpc="l_HPCKit_p_2024.2.1.79_offline.sh"
+install_script_aitools="l_AITools.2024.2.0.156.sh"
+intel_oneapi_BaseToolkits
